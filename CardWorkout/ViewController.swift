0a980c0942e338ffe95b99fa6e57cc4533f37a69
mgKhantt <khantphonenaing156@gamil.com>
2024-10-20 10:10:30 +0630

    Initial Commit

---
+++ CardWorkout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardWorkout
+//
+//  Created by Khant Phone Naing  on 20/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CardWorkoutTests/CardWorkoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardWorkoutTests.swift
+//  CardWorkoutTests
+//
+//  Created by Khant Phone Naing  on 20/10/2024.
+//
+
+import Testing
+@testable import CardWorkout
+
+struct CardWorkoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
